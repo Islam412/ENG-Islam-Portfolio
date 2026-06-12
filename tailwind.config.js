3cf3792e42c757272ac3dae9c7e06d1a3e35b5ef
Islam412 <islam.124.hamdy@gmail.com>
2026-06-12 18:46:21 +0300

    desing tailwind p5

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,8 @@ export default {
         'slide-up': 'slideUp 0.5s ease-out',
         'slide-down': 'slideDown 0.5s ease-out',
         'ping-slow': 'ping 3s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'bounce-slow': 'bounce 2s infinite',
       },
       keyframes: {
         gradient: {
